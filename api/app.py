08d5888d61193ed1640be1b27d3795465351a1a5
DevMaycon <dev.maycon08@gmail.com>
2025-02-22 20:50:39 -0300

    feat(API .env): .env config implemented for API.

--- api/app.py
+++ api/app.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python3
 from flask import Flask
 from flask_cors import CORS
-import routes
+from routes import routes
+from dotenv import load_dotenv
+from os import environ
 
+config = load_dotenv('./configs/.env')
+
+API_HOST, API_PORT = environ.get("api-host"), environ.get('api-port')
+API_ORIGIN=environ.get('api-origin', '')
 
 def main() -> None:
     """Executa a api."""
@@ -14,10 +19,10 @@ def main() -> None:
     routes.configure_routes(flask_app)
     
     # Configuração do Cors
-    CORS(app=flask_app, origins=["https://devmaycon.github.io"])
+    CORS(app=flask_app, origins=[API_ORIGIN])
     
     # Por fim, deploy da aplicação
-    flask_app.run(host='localhost', port=5500)
+    flask_app.run(host=str(API_HOST), port=int(API_PORT))
 
 
 if __name__ == '__main__':
